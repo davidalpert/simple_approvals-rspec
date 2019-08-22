d37706bfc7dbbed65c89e6ef83053c14858ba430
David Alpert <david@spinthemoose.com>
2019-08-22 12:24:30 -0500

    add rake tasks to simplify building and pushing gems

--- rakefile.rb
+++ rakefile.rb
@@ -25,3 +25,19 @@ module SimpleApprovals
     end
   end
 end
+
+task :build do
+  puts 'building simple_approvals-rspec'
+  `gem build ./simple_approvals-rspec.gemspec`
+end
+
+task :publish, [:version] do |_, args|
+  gems = FileList.new('simple_approvals-rspec-*.gem')
+  gem_to_publish = gems.last
+  gem_to_publish = "simple_approvals-rspec-#{args[:version]}.gem" if args[:version]
+  puts "publishing: #{gem_to_publish}"
+
+  raise "cannot find '#{gem_to_publish}'" unless File.file?(gem_to_publish)
+
+  `gem push #{gem_to_publish}`
+end
